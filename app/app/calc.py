7565daff39a90326600c2b8e89087795b2ca4459
Farkhod <mirzabekovfarkhod@gmail.com>
2023-03-16 08:26:50 +0900

    changed

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,10 +1,12 @@
 """
-Calculator fucktions
+Calculator functions
 """
+
 
 def add(x, y):
     """Adds x and y and return the result """
     return x + y
+
 
 def subtract(x, y):
     """Subtract x from y and return the result. """
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,12 +1,14 @@
-""" 
+"""
 Sample test
 """
 from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
+
 
     def test_add_numbers(self):
         """Test adding numbers together. """
@@ -14,8 +16,8 @@ class CalcTests(SimpleTestCase):
 
         self.assertEqual(res, 11)
 
+
     def test_subtract_numbers(self):
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, -5)
-
