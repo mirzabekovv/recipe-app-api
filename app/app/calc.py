76a872e3764ff469bd6efc08490753681af4d928
Farkhod <mirzabekovfarkhod@gmail.com>
2023-03-16 07:27:42 +0900

    CHANGED

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator fucktions
+"""
+
+def add(x, y):
+    """Adds x and y and return the result """
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return the result. """
+    return x - y
